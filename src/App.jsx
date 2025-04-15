cca660f5e8c5c196640772a0ec5cca1b6041a199
Inioluwa1 <adesanyanifesimi@gmail.com>
2025-04-15 14:20:40 +0100

    Barebones of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p className="read-the-docs">
+        Multi Step Form, here we come
+      </p>
+    </div>
+  )
+}
+
+export default App
